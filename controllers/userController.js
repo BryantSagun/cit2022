e79235de1a01d9482b7873d7cd0fa263ab1141f4
Bryant Kyle Sagun <bryantkyle.sagun@gmail.com>
2020-05-16 21:53:24 +0800

    initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+const User = require('../models/User')
+
+exports.login = function(req, res){
+     let user = new User(req.body)
+     user.login().then(function(result){
+          res.send(result)
+     }).catch(function(e){
+          res.send(e)
+     })
+}
+
+exports.logout = function(req, res){
+     
+}
+
+exports.register = function(req, res){
+     let user = new User(req.body)
+     user.register()
+     if(user.errors.length){
+          res.send(user.errors)
+     }
+     else{
+          res.send("Registration Successful.")
+     }
+}
+
+exports.home = function(req, res){
+     res.render('home-guest')
+}
